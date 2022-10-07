60698dcfca012ffb4d20b202caa8b7d099e7a21f
Courdevelops <courdevelops@gmail.com>
2022-10-07 16:44:54 -0400

    js started

--- tasks/freelance.js
+++ tasks/freelance.js
@@ -7,4 +7,22 @@ window.addEventListener('load', () => {
     const input = document.querySelector("#new-task-input");
     // Create the list or tasks element
     const list_el = document.querySelector("#tasks");
+
+    console.log(form);
+
+    form.addEventListener('submit', (e) => {
+        // keeps page from refreshing
+        e.preventDefault();
+
+        console.log("Submitted")
+
+        // create task which is the value of input
+        const task = input.value;
+
+        // Error handling
+        if (!task) {
+            alert("Please enter your freelance job");
+            return;
+        }
+    })
 })
