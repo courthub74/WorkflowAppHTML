150f2573cc1fe23754abc971773071736bf4fff7
Courdevelops <courdevelops@gmail.com>
2022-10-07 16:00:14 -0400

    window DOM started

---
+++ tasks/freelance.js
@@ -0,0 +1,10 @@
+// Load the Window
+window.addEventListener('load', () => {
+    // Once the page is loaded
+    // Create the form element added to the page using the id for the form
+    const form = document.querySelector("#new-task-form");
+    // Create the input element that's entered
+    const input = document.querySelector("#new-task-input");
+    // Create the list or tasks element
+    const list_el = document.querySelector("#tasks");
+})
