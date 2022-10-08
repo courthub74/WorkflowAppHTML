2dee5fe616e303e0bffc197aeb9ad24f9f84e09b
Courdevelops <courdevelops@gmail.com>
2022-10-08 11:24:12 -0400

    ready for button in the DOM

--- tasks/freelance.js
+++ tasks/freelance.js
@@ -24,5 +24,37 @@ window.addEventListener('load', () => {
             alert("Please enter your freelance job");
             return;
         }
+
+        // create task input (DOM Node)
+        const task_el = document.createElement('div');
+
+        // add the task input to the list
+        task_el.classList.add('task');
+
+        // create task input element
+        const task_content_el = document.createElement('div');
+
+        // add the task to the list
+        task_content_el.classList.add('content');
+
+        // append the task content to the task element
+        task_el.appendChild(task_content_el);
+
+        // Create the task input element
+        const task_input_el = document.createElement('input');
+        // set it's class
+        task_input_el.classList.add('text');
+        // set it's type
+        task_input_el.type = 'text';
+        // set it's attribute
+        task_input_el.setAttribute('readonly', 'readonly');
+
+        // append the input to the content
+        task_content_el.appendChild(task_input_el);
+
+        // create the task action element
+        task_actions_el = document.createElement('div');
+        // add task action to the list
+        task_actions_el.classList.add('actions');
     })
 })
