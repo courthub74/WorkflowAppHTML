8d17c98094318ffdcd4c2e447101bf28ffbabea1
Courdevelops <courdevelops@gmail.com>
2022-10-13 18:47:40 -0400

    google auth firebase

--- loginpage/login.js
+++ loginpage/login.js
@@ -1,5 +1,7 @@
 var googlebutton = document.getElementById("googlelogin");
+var provider = new firebase.auth.GoogleAuthProvider();
 
 googlebutton.addEventListener('click', () => {
     console.log("google login clicked");
+    googleSignInPopup(provider);
 })
